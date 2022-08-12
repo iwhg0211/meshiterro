d1ac3a890b0dcbadf82270258afaec2dd7f50e60
EC2 Default User <ec2-user@ip-172-31-24-247.ap-northeast-1.compute.internal>
2022-08-12 09:18:51 +0000

    [Fix]PostimagesコントローラをPostImagesコントローラに修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'postimages/new'
-  get 'postimages/index'
-  get 'postimages/show'
+  resources :post_images, only: [:new, :create, :index, :show]
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
